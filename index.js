4eff3696bac278725863256d8f27cae54162032f
aftelenchana <alejiss401997@gmail.com>
2025-05-15 21:04:37 -0500

    cambio de front

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/scrape", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send("Hola estamos iniciando un servidor para pupeter!");
 });
 
 app.listen(PORT, () => {
